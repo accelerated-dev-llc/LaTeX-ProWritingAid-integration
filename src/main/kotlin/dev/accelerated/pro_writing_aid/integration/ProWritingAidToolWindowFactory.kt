0d604023509711651ce4171f305dd8736378aef1
James Cauwelier <james@accelerated.dev>
2023-06-01 10:12:26 -0700

    update

--- src/main/kotlin/dev/accelerated/pro_writing_aid/integration/ProWritingAidToolWindowFactory.kt
+++ src/main/kotlin/dev/accelerated/pro_writing_aid/integration/ProWritingAidToolWindowFactory.kt
@@ -1,13 +1,15 @@
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.util.ui.JBUI
 
 public class ProWritingAidToolWindowFactory : ToolWindowFactory {
 
+    private val LOG: Logger = Logger.getInstance(ProWritingAidToolWindowFactory::class.java)
+
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        this.thisLogger().debug("Hello tool window!")
+        LOG.debug("Hello tool window!")
         TODO("Not yet implemented")
     }
 }
