0a7e65b6870d7faddfdaeabcda49e77396bed0d4
James Cauwelier <james@accelerated.dev>
2023-06-01 11:36:44 -0700

    Fixed dependencies and displays tool window

--- src/main/kotlin/dev/accelerated/pro_writing_aid/integration/ProWritingAidToolWindowFactory.kt
+++ src/main/kotlin/dev/accelerated/pro_writing_aid/integration/ProWritingAidToolWindowFactory.kt
@@ -1,5 +1,6 @@
+package dev.accelerated.pro_writing_aid.integration
+
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
@@ -9,7 +10,7 @@ public class ProWritingAidToolWindowFactory : ToolWindowFactory {
     private val LOG: Logger = Logger.getInstance(ProWritingAidToolWindowFactory::class.java)
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        LOG.debug("Hello tool window!")
-        TODO("Not yet implemented")
+        LOG.warn("Hello tool window!")
+//        TODO("Not yet implemented")
     }
 }
